b8fedc2a53715593f3b9e97255308493d5cb81a0
Ali <alijfri99@gmail.com>
2021-02-04 14:22:53 +0330

    model.predict is working

--- bonus.py
+++ bonus.py
@@ -3,15 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 import matplotlib.pyplot as plt
 
-cat = get_categories()
-print(cat)
 train = load_texts("train")
-print(train[0])
-s = "dataset/train/images/" + cat[train[0][2]] + "/" + train[0][1]
-print(s)
-a = plt.imread(s)
-plt.imshow(a)
-plt.show()
 train_x, train_y = split_dataset(train)
 test = load_texts("test")
 test_x, test_y = split_dataset(test)
@@ -19,7 +11,7 @@ train_x, test_x = extract_features(train_x, test_x)
 train_x, train_y = reshape(train_x, train_y)
 test_x, test_y = reshape(test_x, test_y)
 
-'''
+
 model = Sequential()
 model.add(Flatten())
 model.add(Dense(32))
@@ -29,5 +21,9 @@ model.add(Dense(19))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(train_x, train_y, epochs=1000, batch_size=32, validation_data=(test_x, test_y))
-'''
+model.fit(train_x, train_y, epochs=20, batch_size=32, validation_data=(test_x, test_y))
+a = model.predict(test_x[1].reshape(1, test_x[0].shape[0]))
+print(a.argmax())
+print(test[1])
+print(get_categories())
+input()
